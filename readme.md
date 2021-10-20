1d8dc7a8809318b9fd87891238bc72e1b1aaddda
Larry Volz <imaginologist@gmail.com>
2021-10-19 21:10:12 -0400

    done

--- readme.md
+++ readme.md
@@ -1,6 +1,13 @@
-# introduction to node assignments
+# Introduction to node assignments
 ## Part One
-- to run enter node step1.js test.html from the cli
+- to run enter node step1.js from the cli
+- for the following argument use test.html  or another file 
 
 ## Part Two
-- 
+- to run enter node step2.js from the cli
+- for the following argument use test.html  or another file or use a URL starting with http
+
+## Part Three
+   to run enter node step2.js from the cli
+- for the following argument use test.html  or another file or use a URL starting with http if you want it output to the console
+- use --out and then the target filename and then the path if you want it written to a file instead
--- step3.js
+++ step3.js
@@ -2,43 +2,57 @@ const fs = require('fs'); //file system
 const process = require('process'); //environment and command line variables
 const axios = require('axios'); 
 
-function cat(path) {
+function handleOutput(data, outputFile){
+    if (outputFile){
+        fs.writeFile(outputFile, data, 'utf8', err =>{
+            if(err){
+                console.error(`Could not write ${outputFile}. Error: ${err}`)
+            }
+        })
+    } else {
+        console.log(data);
+    }
+}
+
+function cat(path, outputFile) {
     fs.readFile(path, 'utf8', function(err, data) {
-    if (err) {
-        // handle possible error
-        console.error("cat threw this error:", err);
-        // kill the process and tell the shell it errored
-        process.exit(1);
-  }
-    // otherwise success
-    console.log(`file contents: ${data}`);
-});
-
-console.log('reading file');
-// file won't have been read yet at this point
-}
-
-async function webCat(path) {
-    try {
-        let resp = await axios.get(path);
-        console.log(resp.data);
-      } catch (err) {
-        console.error(`Error fetching ${path}: ${err}`);
-        process.exit(1);
-      }
-}
-
-
-
-const arg1 = process.argv[2];
-const arg2= process.argv[3];
-
-if (arg1 == '--out'){
+        if (err) {
+            // handle possible error
+            console.error("cat threw this error:", err);
+            // kill the process and tell the shell it errored
+            process.exit(1);
+        }
+        // otherwise success
+        handleOutput(data, outputFile);
+    });
     
-} else if (arg1.slice(0,4) == 'http') {
-    webCat(arg1)
-} else {
-    cat(arg1);
-}
-
-
+ }
+    
+    
+    async function webCat(path, outputFile) {
+        try {
+            let resp = await axios.get(path);
+            handleOutput(resp.data, outputFile)
+        } catch (err) {
+            console.error(`Error fetching ${path}: ${err}`);
+            process.exit(1);
+        }
+    }
+    
+    
+    let path;
+    let outputFile;
+    
+    if (process.argv[2] === '--out'){
+        outputFile = process.argv[3];
+        path = process.argv[4];
+    } else {
+        path = process.argv[2];
+    }
+    
+    if (path.slice(0, 4) === 'http') {
+        webCat(path, outputFile);
+    } else {
+        cat(path, outputFile);
+    }
+    
